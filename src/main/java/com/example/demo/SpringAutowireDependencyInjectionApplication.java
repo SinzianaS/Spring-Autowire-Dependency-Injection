a25e44862d8339fb320aa8105035fc2528496acf
Sinziana Sovejanu <27846889+SinzianaS@users.noreply.github.com>
2022-11-16 17:53:28 +0200

    in progress

--- src/main/java/com/example/demo/SpringAutowireDependencyInjectionApplication.java
+++ src/main/java/com/example/demo/SpringAutowireDependencyInjectionApplication.java
@@ -2,12 +2,16 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringAutowireDependencyInjectionApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringAutowireDependencyInjectionApplication.class, args);
+		
+		Alien a = new Alien();
+		
 	}
 
 }
